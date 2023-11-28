2b98eb238def50a81514f50d46a27f731b14fbb9
Todd Nief <tnief@fe01>
2023-11-28 15:54:33 -0600

    move model to separate file

--- scripts/train.py
+++ scripts/train.py
@@ -19,6 +19,7 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 from datasets import Dataset
 
 from training.utils import inference
+from training.models import MultiTaskConfig, MultiTaskModel
 
 logging.basicConfig(level=logging.INFO)
 
@@ -43,89 +44,89 @@ SAVE_BEST = True
 if SMOKE_TEST:
     MODEL_PATH += "-smoke-test"
 
-class MultiTaskConfig(DistilBertConfig):
-    def __init__(self, classification="linear", num_categories_per_task=None, decoders=None, columns=None, **kwargs):
-        super().__init__(**kwargs)
-        self.num_categories_per_task = num_categories_per_task
-        self.decoders = decoders
-        self.columns = columns
-        self.classification = classification # choices are "linear" or "mlp"
+# class MultiTaskConfig(DistilBertConfig):
+#     def __init__(self, classification="linear", num_categories_per_task=None, decoders=None, columns=None, **kwargs):
+#         super().__init__(**kwargs)
+#         self.num_categories_per_task = num_categories_per_task
+#         self.decoders = decoders
+#         self.columns = columns
+#         self.classification = classification # choices are "linear" or "mlp"
 
-class MultiTaskModel(PreTrainedModel):
-    config_class = MultiTaskConfig
+# class MultiTaskModel(PreTrainedModel):
+#     config_class = MultiTaskConfig
 
-    def __init__(self, config, *args, **kwargs):
-        super().__init__(config)
-        self.distilbert = DistilBertModel(config)
-        self.num_categories_per_task = config.num_categories_per_task
-        self.decoders = json.loads(config.decoders)
-        self.columns = config.columns
-        self.classification = config.classification
+#     def __init__(self, config, *args, **kwargs):
+#         super().__init__(config)
+#         self.distilbert = DistilBertModel(config)
+#         self.num_categories_per_task = config.num_categories_per_task
+#         self.decoders = json.loads(config.decoders)
+#         self.columns = config.columns
+#         self.classification = config.classification
 
-        # TODO: 
-        if self.classification == "mlp":
-            # TODO: wait...the config.dim should be downsampled here probably...
-            self.classification_heads = nn.ModuleList([
-                nn.Sequential(
-                    nn.Linear(config.dim, config.dim // 2),
-                    nn.ReLU(),
-                    nn.Dropout(config.seq_classif_dropout),
-                    nn.Linear(config.dim // 2, num_categories)
-                ) for num_categories in self.num_categories_per_task
-            ])
-        elif self.classification == "linear":
-            self.classification_heads = nn.ModuleList([
-                nn.Sequential(
-                    nn.Linear(config.dim, num_categories),
-                    nn.Dropout(config.seq_classif_dropout)
-                ) for num_categories in self.num_categories_per_task
-            ])
+#         # TODO: 
+#         if self.classification == "mlp":
+#             # TODO: wait...the config.dim should be downsampled here probably...
+#             self.classification_heads = nn.ModuleList([
+#                 nn.Sequential(
+#                     nn.Linear(config.dim, config.dim // 2),
+#                     nn.ReLU(),
+#                     nn.Dropout(config.seq_classif_dropout),
+#                     nn.Linear(config.dim // 2, num_categories)
+#                 ) for num_categories in self.num_categories_per_task
+#             ])
+#         elif self.classification == "linear":
+#             self.classification_heads = nn.ModuleList([
+#                 nn.Sequential(
+#                     nn.Linear(config.dim, num_categories),
+#                     nn.Dropout(config.seq_classif_dropout)
+#                 ) for num_categories in self.num_categories_per_task
+#             ])
 
-    def forward(
-        self,
-        input_ids=None,
-        attention_mask=None,
-        head_mask=None,
-        inputs_embeds=None,
-        labels=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        distilbert_output = self.distilbert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-        )
-        hidden_state = distilbert_output[0]
-        pooled_output = hidden_state[:, 0]
+#     def forward(
+#         self,
+#         input_ids=None,
+#         attention_mask=None,
+#         head_mask=None,
+#         inputs_embeds=None,
+#         labels=None,
+#         output_attentions=None,
+#         output_hidden_states=None,
+#         return_dict=None,
+#     ):
+#         distilbert_output = self.distilbert(
+#             input_ids=input_ids,
+#             attention_mask=attention_mask,
+#             head_mask=head_mask,
+#             inputs_embeds=inputs_embeds,
+#             output_attentions=output_attentions,
+#             output_hidden_states=output_hidden_states,
+#         )
+#         hidden_state = distilbert_output[0]
+#         pooled_output = hidden_state[:, 0]
 
-        logits = [classifier(pooled_output) for classifier in self.classification_heads]
+#         logits = [classifier(pooled_output) for classifier in self.classification_heads]
 
-        loss = None
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            losses = []
-            for logit, label in zip(logits, labels.squeeze().transpose(0, 1)): # trust me
-                losses.append(
-                    loss_fct(logit, label.view(-1))
-                )
-            loss = sum(losses)
+#         loss = None
+#         if labels is not None:
+#             loss_fct = nn.CrossEntropyLoss()
+#             losses = []
+#             for logit, label in zip(logits, labels.squeeze().transpose(0, 1)): # trust me
+#                 losses.append(
+#                     loss_fct(logit, label.view(-1))
+#                 )
+#             loss = sum(losses)
 
-        output = (logits,) + distilbert_output[1:] # TODO why activations? see https://github.com/huggingface/transformers/blob/6f316016877197014193b9463b2fd39fa8f0c8e4/src/transformers/models/distilbert/modeling_distilbert.py#L824
+#         output = (logits,) + distilbert_output[1:] # TODO why activations? see https://github.com/huggingface/transformers/blob/6f316016877197014193b9463b2fd39fa8f0c8e4/src/transformers/models/distilbert/modeling_distilbert.py#L824
     
-        if not return_dict:
-            return (loss,) + output if loss is not None else output
+#         if not return_dict:
+#             return (loss,) + output if loss is not None else output
 
-        return SequenceClassifierOutput(
-            loss=loss,
-            logits=logits,
-            hidden_states=distilbert_output.hidden_states,
-            attentions=distilbert_output.attentions
-        )
+#         return SequenceClassifierOutput(
+#             loss=loss,
+#             logits=logits,
+#             hidden_states=distilbert_output.hidden_states,
+#             attentions=distilbert_output.attentions
+#         )
 
 def read_data(data_path):
     return pl.read_csv(data_path, infer_schema_length=1, null_values=['NULL']).lazy()
