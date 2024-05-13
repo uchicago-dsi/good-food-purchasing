2c1e62fc560dc65cbf7516f9d115796bcb06dc7d
toddnief <todd.nief@gmail.com>
2024-05-13 20:16:54 +0000

    update tests to sample from full dataframe

--- tests/test_v2_pipeline.py
+++ tests/test_v2_pipeline.py
@@ -5,6 +5,7 @@ Beginning refactor with the goal of better code quality.
 """
 
 import pytest
+import pandas as pd
 
 from scripts.pipeline import process_data as initial_process_data
 from scripts.pipeline_v2 import process_data as v2_process_data
@@ -16,14 +17,16 @@ def test_imports():
 
 
 test_args = [
-    # (input_file, raw_data)  # (name of input file with no path, path to folder or test folder acting as raw data source)
-    ("test_input_1.csv", "./tests/assets/"),
-    ("test_input_2.csv", "./tests/assets/"),
+    # (input_file, raw_data, sample)  # (name of input file with no path, path to folder or test folder acting as raw data source, whether to sample)
+    ("test_input_1.csv", "./tests/assets/", False),
+    ("test_input_2.csv", "./tests/assets/", False),
+    ("CONFIDENTIAL_CGFP bulk data_073123.xlsx", "./data/raw/", True),
+    # ("CONFIDENTIAL_CGFP bulk data_073123.xlsx", "./data/raw/", False),
 ]
 
 
-@pytest.mark.parametrize("input_file, raw_data", test_args)
-def test_initial_and_v2_pipelines_match(input_file, raw_data, v2_arg_parser):
+@pytest.mark.parametrize("input_file, raw_data, sample", test_args)
+def test_initial_and_v2_pipelines_match(input_file, raw_data, sample, v2_arg_parser):
     """Setting this up to be parametrized
 
     NOTE: It would be great if we could turn off printing on the initial pipeline version.
@@ -32,6 +35,10 @@ def test_initial_and_v2_pipelines_match(input_file, raw_data, v2_arg_parser):
     See: https://docs.pytest.org/en/7.1.x/example/parametrize.html
     """
     data = load_to_pd(raw_data, input_file)
+
+    if sample:
+        data = data.sample(n=50, random_state=1)
+
     argv = ["--input_file", input_file, "--raw_data", raw_data]
 
     # I don't think args are used after initial setup
