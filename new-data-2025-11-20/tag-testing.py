bdf066c98f93b70ad2445aca0371bfe14c15cf12
Jim Pivarski <jpivarski@gmail.com>
2025-12-19 15:20:03 -0600

    scripts to test all the categories and tags, with and without fine-tuning

---
+++ new-data-2025-11-20/tag-testing.py
@@ -0,0 +1,116 @@
+import json
+import os
+import pathlib
+import threading
+import queue
+import sys
+
+import numpy as np
+import requests
+from tqdm import tqdm
+
+NUM_THREADS = 10
+
+DIRECTORY = pathlib.Path("~/Box/dsi-core/11th-hour/good-food-purchasing/nov2025-dataset/").expanduser()
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+TIMEOUT = 20  # seconds
+TEMPERATURE = 1.0  # randomness from 0 to 2
+
+model, = sys.argv[1:]
+
+with open("tag-instructions.md") as file:
+    tag_instructions = file.read()
+
+with open("tag-schema.json") as file:
+    tag_schema = json.load(file)
+
+tag_testing = []
+with open(DIRECTORY / "fine-tuning-2" / "tag-testing-try1.jsonl") as file:
+    for line in file:
+        tag_testing.append(json.loads(line))
+
+tasks = queue.Queue()
+for message_pair in tag_testing:
+    tasks.put(message_pair["messages"])
+
+for _ in range(NUM_THREADS):
+    tasks.put(None)
+
+output_file = open(f"tag-{model}.jsonl", "w")
+output_lock = threading.Lock()
+
+pbar = tqdm(total=len(tag_testing))
+
+def worker():
+    while True:
+        task = tasks.get()
+        if task is None:
+            break
+
+        user_message, expected_output = task
+
+        try:
+            response = requests.post(
+                "https://api.openai.com/v1/chat/completions",
+                timeout=TIMEOUT,
+                headers={
+                    "Authorization": f"Bearer {OPENAI_API_KEY}",
+                    "Content-Type": "application/json",
+                },
+                json={
+                    "model": model,
+                    "temperature": TEMPERATURE,
+                    "messages": [
+                        {"role": "system", "content": f"""
+Your job is to read a set of food product names and assign tags to them. The input
+is formatted as
+
+```json
+{{"food_products": [...]}}
+```
+
+where each object in `...` contains a food product description in its `"input"`.
+You need to produce a similar JSON object in which each of the output `"food_products"`
+corresponds to one of the inputs, repeating the `"input"` value exactly and adding
+tag attributes as described below.
+
+{tag_instructions}
+""".strip()},
+                        user_message,
+                    ],
+                    "response_format": {
+                        "type": "json_schema",
+                        "json_schema": {
+                            "name": "cgfp-categorization",
+                            "schema": tag_schema,
+                        },
+                    },
+                },
+            )
+        except Exception as err:
+            print(f"{type(err).__name__}: {str(err)}")
+            continue
+
+        try:
+            actual_output = json.loads(response.json()["choices"][0]["message"]["content"])
+        except Exception:
+            print(response.text)
+            continue
+
+        output_line = json.dumps(
+            {"actual": actual_output, "expected": json.loads(expected_output["content"])}
+        ) + "\n"
+        with output_lock:
+            output_file.write(output_line)
+            output_file.flush()
+            pbar.update(1)
+
+threads = [threading.Thread(target=worker) for _ in range(NUM_THREADS)]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+
+pbar.close()
+output_file.close()
